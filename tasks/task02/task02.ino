f756cb3f0c81efa0c317b6550597ff7ae7197959
Варвара Максимова <116665835+varvara007@users.noreply.github.com>
2025-04-09 16:53:10 +0300

    Update task02.ino

--- tasks/task02/task02.ino
+++ tasks/task02/task02.ino
@@ -1,7 +1,32 @@
-void setup() {
+#include <TaskManager.h>
 
+void setup() {
+    pinMode(3,OUTPUT);
+     pinMode(5, OUTPUT);  // sets the pin as output
+      pinMode(6, OUTPUT);  // sets the pin as output
+      pinMode(9, OUTPUT);  // sets the pin as output
+      pinMode(10, OUTPUT);  // sets the pin as output
+    Tasks.add([] {
+         PORTD ^= B00001000;
+    })->startIntervalUsec( 10000);              
+    
+    Tasks.add([] {
+         PORTD ^= B00100000;
+    })->startIntervalUsec( 1000);     
+      
+    Tasks.add([] {
+         PORTD ^= B01000000;
+    })->startIntervalUsec( 500);   
+        
+    Tasks.add([] {
+         PORTB ^= B00000010;
+    })->startIntervalUsec( 100);   
+        
+    Tasks.add([] {
+         PORTB ^= B00000100;
+    })->startIntervalUsec( 50);       
 }
 
 void loop() {
-
+    Tasks.update(); // automatically execute tasks
 }
